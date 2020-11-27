aba093c03a42e39ffc58cf17783d617072e58101
Javier Moreno <mq2880@gmail.com>
2020-11-27 18:48:39 +0100

    Commit

---
+++ ButterfliesShop/Models/Butterfly.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ButterfliesShop.Models
+{
+    public class Butterfly
+    {
+        [Display(Name = "Common Name:")]
+        public int Id { get; set; }
+
+        public string CommonName { get; set; }
+
+        [Display(Name = "Butterfly Family:")]
+        public Family? ButterflyFamily { get; set; }
+        
+        [Display(Name = "Butterflies Quantity:")]
+        public int? Quantity { get; set; }
+
+        [Display(Name = "Characteristics:")]
+        public string Characteristics { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Updated on:")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
+        public DateTime CreatedDate { get; set; }
+        public IFormFile PhotoAvatar { get; set; }
+        public string ImageName { get; set; }
+
+        [Display(Name = "Butterflies Picture:")]
+        public byte[] PhotoFile { get; set; }
+        public string ImageMimeType { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# 20486C-Mod6-Lab-Exercise2
+# Module 6: Developing Models
+
+## Lab: Developing Models
+
+1. **Nombres y apellidos:** Francisco Javier Moreno Quevedo
+2. **Fecha:** 27/11/2020
+3. **Resumen del Ejercicio:** Añadir los componentes necesarios para hacer funcional la tienda de mariposas 2/3 
+4. **Dificultad o problemas presentados y como se resolvieron:** Ninguna
+
+
+
+- Ejercicio 2: Adding a Model
+
+  - Decoramos la clase **Butterfly**
+  - En la pagina principal añadir estos modelos para su visualización
+  - Añadir en su controlador las acciones oportunas
